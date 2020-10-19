550616adde31607bbf79cac475a398e7355df2f5
Anders Bryrup <andersbryrup@gmail.com>
2020-10-19 11:31:59 +0200

    Add button to refresh the webview

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,21 +7,27 @@ import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     var myWebView: WebView? = null
+    var refreshButton: Button? = null
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val context: Context = getApplicationContext()
         myWebView = findViewById(R.id.webview) as WebView?
+        refreshButton = findViewById(R.id.refresh) as Button?
         myWebView?.setWebViewClient(object : WebViewClient() {
 
         })
         myWebView?.loadUrl("https://scratch.mit.edu")
         val webSettings: WebSettings? = myWebView?.getSettings()
         webSettings?.setJavaScriptEnabled(true)
+        refreshButton?.setOnClickListener {
+            myWebView?.loadUrl("https://scratch.mit.edu")
+        }
     }
 }
